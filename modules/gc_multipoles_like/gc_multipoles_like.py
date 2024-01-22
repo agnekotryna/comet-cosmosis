b2593c200b4ac0ccab399c866b1a92d03695891c
Anna Porredon <annam.porredon@gmail.com>
2024-01-22 16:11:56 +0100

    skeleton for likelihood module

---
+++ modules/gc_multipoles_like/gc_multipoles_like.py
@@ -0,0 +1,20 @@
+import numpy as np
+import scipy.interpolate as interp
+from cosmosis.datablock import names, option_section
+
+"""
+Placeholder module for likelihood module
+"""
+
+
+def setup(options):
+
+	#The option section corresponds to the inputs set in the corresponding section of this module in the params.ini file:
+
+	return options
+
+def execute(block, config):
+
+	options = config
+
+	return 0
